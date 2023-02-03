1e43269313688c606719ecfa9f8b25704445f435
Kode102 <48776193+Borinho102@users.noreply.github.com>
2023-02-03 18:23:03 +0100

    UI 90%

--- src/App.js
+++ src/App.js
@@ -96,20 +96,59 @@ function App() {
     }
   ]
 
+  const students = [
+    {
+      id: 1,
+      name: 'Student Max',
+      email: 'max@lms.com',
+      phone: '+237651796157',
+      location: 'Yaounde, Cameroon',
+      score: 10,
+    },
+    {
+      id: 2,
+      name: 'Student Fabrice',
+      email: 'fabrice@lms.com',
+      phone: '+237657758401',
+      location: 'Douala, Cameroon',
+      score: 8.5,
+    },
+    {
+      id: 3,
+      name: 'Student Boris',
+      email: 'boris@lms.com',
+      phone: '+237657798765',
+      location: 'Bafoussam, Cameroon',
+      score: 7,
+    }
+  ]
+
   const [teacherData, setTeacherData] = useState(teachers)
+
+  const [studentData, setStudentData] = useState(students)
 
   const deleteTeacher = (id) => {
     setTeacherData(teacherData.filter((teacher) => teacher.id !== id))
   }
 
+  const deleteStudent = (id) => {
+    setStudentData(studentData.filter((student) => student.id !== id))
+  }
+
   const { isOpen: isDelOpen, onOpen: onDelOpen, onClose: onDelClose } = useDisclosure()
   const { isOpen: isAddOpen, onOpen: onAddOpen, onClose: onAddClose } = useDisclosure()
+
+  const { isOpen: isDel2Open, onOpen: onDel2Open, onClose: onDel2Close } = useDisclosure()
+  const { isOpen: isAdd2Open, onOpen: onAdd2Open, onClose: onAdd2Close } = useDisclosure()
+
   const cancelRef = useRef()
+  const cancel2Ref = useRef()
 
   const [id, setID] = useState(null)
 
   const [name, setName] = useState("")
   const [role, setRole] = useState("")
+  const [score, setScore] = useState(0)
   const [email, setEmail] = useState("")
   const [phone, setPhone] = useState("")
   const [loation, setLocation] = useState("")
@@ -125,6 +164,7 @@ function App() {
     switch(name) {
       case 'name': setName(value); break;
       case 'role': setRole(value); break;
+      case 'score': setRole(value); break;
       case 'email': setEmail(value); break;
       case 'phone': setPhone(value); break;
       case 'location': setLocation(value); break;
@@ -613,7 +653,213 @@ function App() {
               </TabPanel>
 
               <TabPanel>
-                <p>three!</p>
+                <div className="py-5">
+
+                  <h2 className="fw-bold text-center fs-4">
+                    Students Records
+                  </h2>
+
+                  <TableContainer className='mt-5 border rounded-3'>
+                    <Table variant='simple'>
+                      <TableCaption className='link pb-4'>All Students Records</TableCaption>
+
+                      <Thead className='bg-dark'>
+                        <Tr>
+                          <Th className='text-white'>Full Name</Th>
+                          <Th className='text-white'><center>Score</center></Th>
+                          <Th className='text-white text-center'>Contact</Th>
+                          <Th className='text-white text-center'>
+
+                            <Button onClick={onAdd2Open} className='w-75' leftIcon={<ion-icon name="add-outline"></ion-icon>} colorScheme='blue' size='sm' variant='solid'>
+                              Add record
+                            </Button>
+                            <Modal size={'3xl'} closeOnOverlayClick={false} isOpen={isAdd2Open} onClose={onAdd2Close} isCentered>
+                              <ModalOverlay />
+                              <ModalContent>
+                                <ModalHeader>Add New Student</ModalHeader>
+                                <ModalCloseButton />
+                                <ModalBody>
+
+                                  <Alert
+                                    status='info'
+                                    variant='subtle'
+                                    flexDirection='column'
+                                    alignItems='center'
+                                    justifyContent='center'
+                                    textAlign='center'
+                                    height='150px'
+                                    className='mb-3'>
+                                    <AlertIcon boxSize='40px' mr={0} />
+                                    <AlertTitle mt={4} mb={1} fontSize='lg'>
+                                      Create Teacher Record
+                                    </AlertTitle>
+                                    <AlertDescription maxWidth='sm'>
+                                      Enter your valid information to save a new record
+                                    </AlertDescription>
+                                  </Alert>
+
+                                  <div className='row'>
+                                    <div className='col-md-6'>
+                                      <InputGroup size='lg' className="mb-4">
+                                        <InputLeftElement
+                                          pointerEvents='none'
+                                          children={
+                                            <ion-icon name="person-outline"></ion-icon>
+                                          }
+                                        />
+                                        <Input type='text' placeholder='Full Name' />
+                                      </InputGroup>
+                                    </div>
+                                    <div className='col-md-6'>
+                                      <Select size='lg' placeholder='Select Role'>
+                                        <option value='teacher'>Teacher</option>
+                                        <option value='principal'>Supervisor</option>
+                                        <option value='other'>Other</option>
+                                      </Select>
+                                    </div>
+                                  </div>
+
+                                  <div className='row'>
+                                    <div className='col-md-6'>
+                                      <InputGroup size='lg' className="mb-4">
+                                        <InputLeftElement
+                                          pointerEvents='none'
+                                          children={
+                                            <ion-icon name="mail-outline"></ion-icon>
+                                          }
+                                        />
+                                        <Input type='email' placeholder='Email Address' />
+                                      </InputGroup>
+                                    </div>
+                                    <div className='col-md-6'>
+                                      <InputGroup size='lg' className="mb-4">
+                                        <InputLeftElement
+                                          pointerEvents='none'
+                                          children={
+                                            <ion-icon name="call-outline"></ion-icon>
+                                          }
+                                        />
+                                        <Input type='tel' placeholder='Phone Number' />
+                                      </InputGroup>
+                                    </div>
+                                  </div>
+
+                                  <div className='row'>
+                                    <div className='col-md-12'>
+                                      <InputGroup size='lg' className="mb-4">
+                                        <InputLeftElement
+                                          pointerEvents='none'
+                                          children={
+                                            <ion-icon name="location-outline"></ion-icon>
+                                          }
+                                        />
+                                        <Input type='text' placeholder='Location Address' />
+                                      </InputGroup>
+                                    </div>
+                                  </div>
+
+                                </ModalBody>
+
+                                <ModalFooter>
+                                  <Button colorScheme='red' mr={3} onClick={onAdd2Close}>
+                                    Close
+                                  </Button>
+                                  <Button rightIcon={<ion-icon name="add-outline"></ion-icon>} colorScheme='blue'>Add Teacher</Button>
+                                </ModalFooter>
+                              </ModalContent>
+                            </Modal>
+
+                          </Th>
+                        </Tr>
+                      </Thead>
+
+                      <Tbody>
+                        {
+                          studentData.map((student, index) => {
+                            return (
+                              <Tr key={index}>
+                                <Td>{student.name}</Td>
+                                <Td><center>{student.score}</center></Td>
+                                <Td className=" text-center">
+                                  <a href={"mailto:" + student.email}>
+                                    <Tooltip label={'Write to ' + student.email}><ion-icon size="large" name="mail-outline"></ion-icon></Tooltip>
+                                  </a>
+                                  <a href={"tel:" + student.phone} className="mx-4">
+                                    <Tooltip label={'Call ' + student.phone}><ion-icon size="large" name="call-outline"></ion-icon></Tooltip>
+                                  </a>
+                                  <span>
+                                    <Tooltip label={student.location}><ion-icon size="large" name="location-outline"></ion-icon></Tooltip>
+                                  </span>
+                                </Td>
+                                <Td className="text-center">
+                                  <Button className='w-50' size='sm' leftIcon={<ion-icon name="create-outline"></ion-icon>} colorScheme='yellow' variant='solid'>
+                                    Update
+                                  </Button>
+
+                                  &nbsp;
+
+                                  <Button onClick={() => {
+                                    setID(student.id)
+                                    onDel2Open()
+                                  }} className='w-25' size='sm' leftIcon={<ion-icon name="trash-outline"></ion-icon>} colorScheme='red' variant='solid'>
+                                    Detele
+                                  </Button>
+
+                                  <AlertDialog
+                                    motionPreset='slideInBottom'
+                                    leastDestructiveRef={cancel2Ref}
+                                    onClose={onDel2Close}
+                                    isOpen={isDel2Open}
+                                    isCentered
+                                  >
+                                    <AlertDialogOverlay />
+
+                                    <AlertDialogContent>
+                                      <AlertDialogHeader>Delete Record?</AlertDialogHeader>
+                                      <AlertDialogCloseButton />
+                                      <AlertDialogBody>
+                                        Are you sure you want to delete record?
+                                      </AlertDialogBody>
+                                      <AlertDialogFooter>
+                                        <Button ref={cancel2Ref} onClick={onDel2Close}>
+                                          No
+                                        </Button>
+                                        <Button onClick={() => {
+                                          deleteStudent(id)
+                                          onDel2Close()
+                                          setMessage({
+                                            type: 'success',
+                                            title: 'Delete!',
+                                            desc: 'Record deleted successfully',
+                                          })
+                                        }
+                                        } colorScheme='red' ml={3}>
+                                          Yes
+                                        </Button>
+                                      </AlertDialogFooter>
+                                    </AlertDialogContent>
+                                  </AlertDialog>
+
+                                </Td>
+                              </Tr>
+                            )
+                          })
+                        }
+                      </Tbody>
+                      <Tfoot className='bg-dark'>
+                        <Tr>
+                          <Th className='text-white'>Full Name</Th>
+                          <Th className='text-white'><center>Score</center></Th>
+                          <Th className='text-white text-center'>Contact</Th>
+                          <Th className='text-white text-center'>
+                            Actions
+                          </Th>
+                        </Tr>
+                      </Tfoot>
+                    </Table>
+                  </TableContainer>
+
+                </div>
               </TabPanel>
 
             </TabPanels>
